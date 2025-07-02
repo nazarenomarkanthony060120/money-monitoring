9bb9a4790544b7c2b13b039f118084b34c7940a5
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2025-07-02 13:53:19 +0800

    Fix error build in java sdk

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -3,6 +3,7 @@ import { Stack } from 'expo-router'
 import { StatusBar } from 'expo-status-bar'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import '../global.css'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
 // Create a client
 const queryClient = new QueryClient({
@@ -16,22 +17,24 @@ const queryClient = new QueryClient({
 
 export default function RootLayout() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Stack.Screen
-          name="index"
-          options={{
-            statusBarHidden: true,
-            statusBarStyle: 'light',
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <QueryClientProvider client={queryClient}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
           }}
-        />
-        <Stack.Screen name="(tabs)" />
-      </Stack>
-      <StatusBar style="auto" />
-    </QueryClientProvider>
+        >
+          <Stack.Screen
+            name="index"
+            options={{
+              statusBarHidden: true,
+              statusBarStyle: 'light',
+            }}
+          />
+          <Stack.Screen name="(tabs)" />
+        </Stack>
+        <StatusBar style="auto" />
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   )
 }
